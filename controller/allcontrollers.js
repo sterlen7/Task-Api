615dd1e689ff705ccec5ed8cf3e846a9b79dfb0b
sterlen7 <infosterlen@gmail.com>
2024-03-21 20:42:12 +0000

    new

--- controller/allcontrollers.js
+++ controller/allcontrollers.js
@@ -1,32 +1,53 @@
 const tasks = [
-   { id: 1, taskTitle: 'Eat' },
-   { id: 2, taskTitle: 'code' },
-   { id: 3, taskTitle: 'code' },
+   { id: 1, taskTitle: 'Eat',createdAt:new Date(),completed :false },
+   { id: 2,  taskTitle: 'code',createdAt:new Date(),completed :false  },
+   { id: 3, taskTitle: 'sleep' , createdAt:new Date(),completed :false },
  ];
  
 
-function getAll(req, res) {   //controller for getting all tasks 
+function getAll(req, res){   // for getting all tasks 
    res.json(tasks);
  }
 
  function addNew(req,res){  // for adding new tasks 
-    const newTask = {
-      id: 4,
-      taskTitle: 'Send Email',
-    };
+    const newTask = {id: 4,taskTitle: 'Send Email',createdAt:new Date(),completed :false };
     tasks.push(newTask);
     res.json(tasks)
  }
 
+ function getOneTask(req, res){ // for single tasks
+   const taskId = parseInt(req.params.id); 
+   const task = tasks.find((task) => task.id === taskId);
+ 
+   if (task) {
+     res.json(task);
+   } else {
+     res.status(404).json({ message: "Task not found" }); 
+   }
+
+ }
+
+
+function deleteTask(req, res){// to delete task
+   const taskId = parseInt(req.params.id); 
+    const taskToDelete = tasks.find((task)=> task.id === taskId);
+    
+    if (taskToDelete !== -1) {
+      tasks.splice(taskToDelete, 1);
+      res.json('Task deleted'); 
+      
+   }else{
+      res.status(404).send("Task not found"); 
+   }
+  
+}
 
 
 
+module.exports= {getAll,addNew,getOneTask,deleteTask}
 
-//  function delete
+// module.exports=deleteTask
 
 
-
-module.exports= getAll
-module.exports= addNew
 
 
--- routes/allRoutes.js
+++ routes/allRoutes.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const getAll = require('../controller/allcontrollers');
-const addNew = require('../controller/allcontrollers');
-
-
-
-
+const {getAll,addNew, getOneTask,deleteTask} = require('../controller/allcontrollers');
 const allRoutes = express.Router();
 
 
 allRoutes.get('/all',getAll)// route for getting all tasks 
-allRoutes.get('/new',addNew)
-
-
-
-
+allRoutes.post('/new',addNew)//route for adding new task
+allRoutes.get('/task/id:',getOneTask)//route to get one task
+allRoutes.delete('/delete/id:',deleteTask) //route to delete task
 
 
 
